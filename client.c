ab58851029d2380341e1b040c397ad6d778e219c
danteire <pioter814@gmail.com>
2024-06-27 21:44:34 +0200

    added sysos check before in macros before executing code // no winapi implementation yet

--- client.c
+++ client.c
@@ -1,16 +1,28 @@
 // C program for the Client Side
+//
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
 
+#ifdef _WIN32
+
+#include <windows.h>
+
+#else
+
+#include <sys/socket.h>
 // inet_addr
 #include <arpa/inet.h>
 #include <unistd.h>
-
 // For threading, link with lpthread
 #include <pthread.h>
 #include <semaphore.h>
+#endif
+
+
+
+
+
 
 void* clienthread(void* args);
 
@@ -20,27 +32,59 @@ void* clienthread(void* args){
     int network_socket;
 
     //tworzenie socketu strumienia
+
+    #ifdef _WIN32
+
+
+
+    #else
+
     network_socket = socket(AF_INET,SOCK_STREAM,0);
 
     struct sockaddr_in servAddr;
     servAddr.sin_port = htons(9001);
     servAddr.sin_addr.s_addr = INADDR_ANY;
     servAddr.sin_family = AF_INET;
+    #endif
+
+
+
+
 
     //inicjuj poleczenie
+    #ifdef _WIN32
 
-    int connectionStatus = connect(network_socket,(struct sockaddr*)&servAddr, sizeof(servAddr));
 
-    if(connectionStatus<0){
-        printf("CONNECTION ERROR\n");
-        return 0;
-    }
+
+    #else
+
+        int connectionStatus = connect(network_socket,(struct sockaddr*)&servAddr, sizeof(servAddr));
+
+        if(connectionStatus<0){
+            printf("CONNECTION ERROR\n");
+            return 0;
+        }
+
+    #endif
+
+
 
     printf("CONNECTED!\n");
-    send(network_socket,&client_reqest, sizeof(client_reqest),0);
 
-    close(network_socket);
-    pthread_exit(NULL);
+
+
+    #ifdef _WIN32
+
+
+
+    #else
+        send(network_socket,&client_reqest, sizeof(client_reqest),0);
+
+        close(network_socket);
+        pthread_exit(NULL);
+
+    #endif
+
 
     return 0;
 }
@@ -56,23 +100,49 @@ int main() {
         return 1;
     }
 
+    #ifdef _WIN32
+
+
+
+    #else
+
     pthread_t tid;
+
+    #endif
+
 
     switch (choice) {
         case 1:{
             int clientRequest = 1;
 
-            //tworze thread
-            pthread_create(&tid,NULL,clienthread,&clientRequest);
-            sleep(20);
-            break;
-        }
-        case 2:{
-            int clientRequest = 2;
+            #ifdef _WIN32
+
+
+
+            #else
 
             //tworze thread
             pthread_create(&tid,NULL,clienthread,&clientRequest);
             sleep(20);
+
+            #endif
+
+            break;
+        }
+        case 2:{
+            int clientRequest = 2;
+            #ifdef _WIN32
+
+
+
+            #else
+
+            //tworze thread
+            pthread_create(&tid,NULL,clienthread,&clientRequest);
+            sleep(20);
+
+            #endif
+
             break;
         }
         default:{
@@ -80,7 +150,15 @@ int main() {
             break;
         }
     }
+        #ifdef _WIN32
 
-    pthread_join(tid,NULL);
+
+
+        #else
+
+        pthread_join(tid,NULL);
+
+        #endif
+
     return 0;
 }
--- server.c
+++ server.c
@@ -1,27 +1,66 @@
 // C program for the Server Side
 
-// inet_addr
-#include <arpa/inet.h>
+
 
 // For threading, link with lpthread
+#ifdef _WIN32
+
+#include <winsock2.h>
+#include <windows.h>
+
+
+#else
+
 #include <pthread.h>
 #include <semaphore.h>
+#include <sys/socket.h>
+// inet_addr
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
+
+
+
 
 // Semaphore variables
+
+#ifdef _WIN32
+
+
+
+#else
+
 sem_t x, y;
 pthread_t tid;
 pthread_t writerthreads[100];
 pthread_t readerthreads[100];
+
+#endif
+
+
+
+
 int readercount = 0;
 
 void* reader(void* param);
 void* writer(void* param);
 
+#ifdef _WIN32
+
+void* reader(void* param){
+
+    return 0;
+}
+void* writer(void* param){
+
+    return 0;
+}
+#else
 void* reader(void* param){
     //blokowanie semaphora
     sem_wait(&x);
@@ -61,55 +100,135 @@ void* writer(void* param){
     printf("\n Writer wychodzi!");
     pthread_exit(NULL);
 }
+#endif
+
+
+#ifdef _WIN32
+
+
+
+#else
+
+
+
+#endif
 
 int main(){
-    // Initialize variables
-    int serverSocket, newSocket;
-    struct sockaddr_in serverAddr;
-    struct sockaddr_storage serverStorage;
 
-    socklen_t addr_size;
-    sem_init(&x,0,1);
-    sem_init(&y,0,1);
 
-    serverSocket = socket(AF_INET,SOCK_STREAM,0);
-    serverAddr.sin_addr.s_addr= INADDR_ANY;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(9001);
+    #ifdef _WIN32
+
+
+
+    #else
+        // Initialize variables
+        int serverSocket, newSocket;
+        struct sockaddr_in serverAddr;
+        struct sockaddr_storage serverStorage;
+
+        socklen_t addr_size;
+        sem_init(&x,0,1);
+        sem_init(&y,0,1);
+
+        serverSocket = socket(AF_INET,SOCK_STREAM,0);
+
+        serverAddr.sin_addr.s_addr= INADDR_ANY;
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(9001);
+    #endif
+
+
+
+
+
+
+    #ifdef _WIN32
+
+
+
+    #else
 
     //bind socket do adresu i portu
 
     bind(serverSocket,(struct sockaddr*)&serverAddr, sizeof(serverAddr));
 
     // Listen on the socket, with 40 max connection requests queued
-    if (listen(serverSocket, 50) == 0){
-        printf("Listening\n");
-    }else{
-        printf("Error\n");
-    }
+        if (listen(serverSocket, 50) == 0){
+            printf("Listening\n");
+        }else{
+            printf("Error\n");
+        }
+    #endif
 
-    pthread_t tid[60];
+
+
+    #ifdef _WIN32
+
+
+
+    #else
+
+        pthread_t tid[60];
+
+    #endif
+
     int i =0;
     while (1){
-        addr_size = sizeof(serverStorage);
+            #ifdef _WIN32
 
-        // Extract the first connection in the queue
-        newSocket = accept(serverSocket,(struct sockaddr*)&serverStorage,&addr_size);
+
+
+            #else
+
+                    addr_size = sizeof(serverStorage);
+
+                    // Extract the first connection in the queue
+                    newSocket = accept(serverSocket,(struct sockaddr*)&serverStorage,&addr_size);
+
+            #endif
+
 
         int choice = 0;
-        recv(newSocket,&choice, sizeof(choice), 0);
+        #ifdef _WIN32
+
+
+
+        #else
+
+            recv(newSocket,&choice, sizeof(choice), 0);
+
+        #endif
+
 
         if(choice==1){
-            //nowe poloczenie, tworze thread
-            if(pthread_create(&readerthreads[i++],NULL,reader,&newSocket)!=0){
-                // Error in creating thread
-                printf("Failed to create thread\n");
-            }
+            #ifdef _WIN32
+
+
+
+            #else
+
+                //nowe poloczenie, tworze thread
+                if(pthread_create(&readerthreads[i++],NULL,reader,&newSocket)!=0){
+                    // Error in creating thread
+                    printf("Failed to create thread\n");
+                }
+
+            #endif
+
         }else if(choice==2){
-            if(pthread_create(&writerthreads[i++],NULL,writer,&newSocket)!=0){
-                // Error in creating thread
-                printf("Failed to create thread\n");
-            }
+            #ifdef _WIN32
+
+
+
+            #else
+
+                        if(pthread_create(&writerthreads[i++],NULL,writer,&newSocket)!=0){
+                            // Error in creating thread
+                            printf("Failed to create thread\n");
+                        }
+
+            #endif
+
         }
 
         if(i>=50){
@@ -117,8 +236,17 @@ int main(){
             i=0;
 
             while (i<50){
-                pthread_join(writerthreads[i++],NULL);
-                pthread_join(readerthreads[i++],NULL);
+                #ifdef _WIN32
+
+
+
+                #else
+
+                    pthread_join(writerthreads[i++],NULL);
+                    pthread_join(readerthreads[i++],NULL);
+
+                #endif
+
             }
 
             i=0;
